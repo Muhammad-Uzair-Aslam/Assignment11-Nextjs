f7c8e7365d3e15af271f6b395c0ad172e85984f9
Uzair Aslam <uzairaslam990@gmail.com>
2023-09-01 03:42:26 -0700

    About page added

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import AboutUsSection from '../(components)/aboutUsSection/aboutUsSection'
 
 export default function About() {
   return (
     <div>
-      
+      <AboutUsSection/>
     </div>
   )
 }
